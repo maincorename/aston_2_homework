0ceefe3a528bad184737438ae225f921f466e9b5
Андриянов Роман <maincorename@github.com>
2026-05-23 14:14:46 +0700

    Добавлено подключение Hibernate

--- src/main/java/util/HibernateUtil.java
+++ src/main/java/util/HibernateUtil.java
@@ -1,4 +1,27 @@
 package util;
 
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
 public class HibernateUtil {
+    private static final SessionFactory sessionFactory = buildSessionFactory();
+
+    private static SessionFactory buildSessionFactory() {
+        try {
+            // Загрузка конфигурации из hibernate.cfg
+            return new Configuration().configure().buildSessionFactory();
+        } catch (Throwable e) {
+            System.out.println("Произошла ошибка при создании SessionFactory" + e);
+            // Бросаем исключение для трассировки ошибки
+            throw new ExceptionInInitializerError(e);
+        }
+    }
+
+    public static SessionFactory getSessionFactory() {
+        return sessionFactory;
+    }
+
+    public static void shutdown() {
+        getSessionFactory().close();
+    }
 }
