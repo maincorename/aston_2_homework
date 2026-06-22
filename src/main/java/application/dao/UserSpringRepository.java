ccd75441b09b0f053877788135820bcbd02e8617
Roman <52419275+maincorename@users.noreply.github.com>
2026-06-23 01:05:06 +0700

    kafka-integration (#3)
    * Разделил на микросервисы NotificationService и UserService
    Добавлены Dockerfile и docker-compose
    ---------
    
    Co-authored-by: Андриянов Роман <maincorename@github.com>

--- src/main/java/application/dao/UserSpringRepository.java
+++
@@ -1,7 +0,0 @@
-package application.dao;
-
-import application.entity.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserSpringRepository extends JpaRepository<User, Long> {
-}
--- src/main/java/application/dto/UserDto.java
+++
@@ -1,3 +0,0 @@
-package application.dto;
-
-public record UserDto (Long id, String name, String email, Integer age) { }
